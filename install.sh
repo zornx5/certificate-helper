658785345cdc352d0f563a00a2c1e2982cf1b644
zornx5 <zornx5@gmail.com>
2023-04-04 17:51:28 +0800

    build: 移除 Maven 管理

--- install.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-mvn clean source:jar javadoc:javadoc install -Dmaven.test.skip=false -Dmaven.javadoc.skip=false
--- package.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-mvn clean source:jar javadoc:javadoc package -Dmaven.test.skip=false -D maven.javadoc.skip.true
